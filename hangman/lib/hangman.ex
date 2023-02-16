05681a91b17a81539f56dd761d22a726246b104e
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2023-02-16 15:31:31 +0100

    refactor code

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,30 +1,14 @@
 defmodule Hangman do
 
 alias Hangman.Impl.Game, as: Game
-# equivalent to syntax above, by default alias will use the last name
-# alias Hangman.Impl.Game
+alias Hangman.Type
 
-# @opaque defines a type whose contents are not usable outside this module (this is how we secure internal state)
-
-@type state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
 @opaque game :: Game.t
-@type tally :: %{
-  turns_left: integer,
-  game_state: state,
-  letters: list(String.t),
-  letters_used: list(String.t)
-}
 
 @spec new_game() :: game
-
 defdelegate new_game, to: Game
 
-#  below code is equivalent to line above
-  # def new_game do
-  #   Game.new_game()
-  # end
-
-@spec make_move(game, String.t) :: { game, tally }
+@spec make_move(game, String.t) :: { game, Type.tally }
   def make_move(_game, _guess) do
   end
 end
--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -1,17 +1,14 @@
 defmodule Hangman.Impl.Game do
 
-# we can also convert it into a type by adding "@type t"
-# this way type will have same name as defmodule
+alias Hangman.Type
 
 @type t :: %__MODULE__{
   turns_left: integer,
-  game_state: Hangman.state,
+  game_state: Type.state,
   letters: list(String.t),
   letters_used: MapSet.t(String.t)
 }
 
-# this is a structure for the hangman game, it can be refered to as 'Hangman.Impl.Game'
-# structure will always have the name of the module
 defstruct(
   turns_left: 7,
   game_state: :initializing,
@@ -19,30 +16,12 @@ defstruct(
   letters_used: MapSet.new()
 )
 
-#
-
-  # def new_game do
-    #  this is our internal state and will be initialized with the given values
-    #  this will return the state to the outside world
-
-    # we can also override the default values if needed
-
-    # %__MODULE__{
-    #   letters: Dictionary.random_word |> String.codepoints
-    # }
-    # below is equivalent to above expression
-    # %Hangman.Impl.Game{
-    #   letters: Dictionary.random_word |> String.codepoints
-    # }
-
-  # end
-
-#
-
+  @spec new_game() :: t
   def new_game do
     new_game(Dictionary.random_word)
   end
 
+  @spec new_game(String.t) :: t
   def new_game(word) do
     %__MODULE__{
       letters: word |> String.codepoints
